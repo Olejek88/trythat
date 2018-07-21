963de1ef7d056e6572a91cf897a864aa1e41fd03
Olejek <olejek8@yandex.ru>
2018-07-21 15:58:36 +0500

    Add activity photo & description

--- src/components/Activity/ActivityDescription.js
+++ src/components/Activity/ActivityDescription.js
@@ -5,6 +5,32 @@ import { observer } from 'mobx-react';
 class ActivityDescription extends React.Component {
   render() {
     return (
+        <div className="left-box desc-box">
+            <div className="pdp-product-description sg-f-bdy">
+                <h4 className="sg-f-ttl">The Experience</h4>
+                <div className="product-desc body-text" style="position: relative; word-wrap: break-word;">
+                    <p>Chez Panisse is a dining institution. Prior to Alice Waters
+                        opening her venerated restaurant, home cooking typically involved highly
+                        processed foods and fine dining required complicated French techniques.
+                        Then Chez Panisse with its local sourced meats, farm-to-table produce,
+                        and vibrant but simple presentations pioneered a new approach to how
+                        America looked at dining. This new California cuisine would change how
+                        the entire country eats. &nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>For your next milestone meal, visit the humble, yet groundbreaking
+                        kitchen as an honored guest. Many consider this intimate table best seat
+                        in the house, nearly impossible to book until now. Throughout your
+                        meal, you will be immersed in a beautifully choreographed production led
+                        by restaurant chef Jennifer Sherman. The menu changes nightly and is
+                        designed to reflect the season and represent the best of what local
+                        farmers are growing. This celebratory meal, in the heart of the buzzing
+                        Chez Panisse kitchen, will be a revelatory experience that you will
+                        never forget.</p>
+                </div>
+            </div>
+            <div className="pdp-shipping-details">
+            </div>
+        </div>
     );
   }
 }
--- src/components/Activity/ActivityPhoto.js
+++ src/components/Activity/ActivityPhoto.js
@@ -8,43 +8,42 @@ class ActivityPhoto extends React.Component {
         <div className="left-box product-slider-box" style="margin-bottom: 40px;">
             <div className="pImgContainer">
                 <!-- "previous slide" button -->
-                <a className="prev p-arrow disabled" id="pImages_left" tabIndex="0"></a>
+                <a className="prev p-arrow disabled" id="pImages_left" tabIndex="0">
+                </a>
                 <div className="scrollable-placeholder" style="display: none;">
-                    <img
-                        src="https://d5xydlzdo08s0.cloudfront.net/media/celebrities/36/products/chezpanisse_restaurant_1__l1__L.jpg"
-                        alt="Dine in the Heart of the Chez Panisse Kitchen: A Convivial Supper at an Iconic Restaurant: In Berkeley, California"
-                        style="max-width: 714px; width: 100%; display: block; margin: 0 auto;">
+                    <img src="ex1.jpg" alt="Experience" style="max-width: 714px; width: 100%; display: block; margin: 0 auto;"/>
                 </div>
                 <div className="scroll" style="display: block;">
                     <div className="inner-container items" style="width: 100000px; left: -714px; height: 470px;">
                         <div className="imgDiv cloned" style="">
-                            <img data-link="example_experience.jpg" src="/example_experience.jpg"
-                                title="Dine in the Heart" alt="Dine in the Heart"
+                            <img data-link="example_experience.jpg" src="ex2.jpg"
+                                title="Example Experience" alt="Example Experience"
                                 style="max-width: 714px; width: 100%; height: 100%; display: block; margin: 0 auto;" />
                         </div>
                         <div className="imgDiv" style="position: relative; visibility: visible; margin-top: 0px;">
-                            <img data-link="example_experience.jpg" src="/example_experience.jpg"
-                                 title="Dine in the Heart" alt="Dine in the Heart"
+                            <img data-link="example_experience.jpg" src="ex3.jpg"
+                                 title="Example Experience" alt="Example Experience"
                                  style="max-width: 714px; width: 100%; height: 100%; display: block; margin: 0 auto;" />
                         </div>
                         <div className="imgDiv cloned"
                              style="position: relative; visibility: visible; margin-top: 0px;">
-                            <img data-link="example_experience.jpg" src="/example_experience.jpg"
-                                 title="Dine in the Heart" alt="Dine in the Heart"
+                            <img data-link="example_experience.jpg" src="ex4.jpg"
+                                 title="Example Experience" alt="Example Experience"
                                  style="max-width: 714px; width: 100%; height: 100%; display: block; margin: 0 auto;" />
                         </div>
                     </div>
                 </div>
-                <a className="next p-arrow disabled" id="pImages_right" tabIndex="0"></a>
+                <a className="next p-arrow disabled" id="pImages_right" tabIndex="0">
+                </a>
             </div>
 
             <div style="width: 100%; position: absolute; bottom: 0px;">
                 <!-- the tabs -->
                 <div className="pImgSlide" style="margin: 0 auto; width: 14px;">
-                    <a href="#" className="active"></a>
+                    <a href="#" className="active">
+                    </a>
                 </div>
             </div>
-            <div className="sweepsSymbol sg-text-transform " style="display:none;"><p></p></div>
         </div>
   );
   }
